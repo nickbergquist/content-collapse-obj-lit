c12c6bf98a4a6d4100d3485baa3bbe17be62f0c7
Nick Bergquist <nick_bergquist@yahoo.co.uk>
2018-02-06 12:50:13 +0000

    Add local scope and immediate execution avoiding page flash

--- script/main.js
+++ script/main.js
@@ -1,122 +1,120 @@
-//var $ = window.jQuery;
 document.documentElement.className += ' js';
 
-var listControl = {
-    'config': {
-        'container' : $('.list-control')
-    },
-    'init': function(config) {
-        // 1. enable any custom config object to be merged
-        if (config && typeof(config) == 'object') {
-            $.extend(listControl.config, config);
-        }
+// create local scope with a self executing anonymous function
+(function() {
+    var listControl = {
+        'config': {
+            'container' : $('.list-control')
+        },
+        'init': function(config) {
+            // 1. enable any custom config object to be merged
+            if (config && typeof(config) == 'object') {
+                $.extend(listControl.config, config);
+            }
 
-        // 2. cached/created DOM elements for further use
-        listControl.$container = listControl.config.container;        
-        listControl.$sections = listControl.$container.find('ul.sections > li');
-        
-        // section nav
-        listControl.$navSection = $('<ul/>')
-            .attr('class', 'nav-section')
-            .prependTo(listControl.$container);
-        
-        // item nav
-        listControl.$itemNav = $('<ul/>')
-            .attr('class', 'nav-item')
-            .insertAfter(listControl.$navSection);
-        
-        // item content
-        listControl.$content = $('<p/>')
-            .attr('class', 'content')
-            .insertAfter(listControl.$itemNav);
+            // 2. cached/created DOM elements for further use
+            listControl.$container = listControl.config.container;        
+            listControl.$sections = listControl.$container.find('ul.sections > li');
+            
+            // section nav
+            listControl.$navSection = $('<ul/>')
+                .attr('class', 'nav-section')
+                .prependTo(listControl.$container);
+            
+            // item nav
+            listControl.$itemNav = $('<ul/>')
+                .attr('class', 'nav-item')
+                .insertAfter(listControl.$navSection);
+            
+            // item content
+            listControl.$content = $('<p/>')
+                .attr('class', 'content')
+                .insertAfter(listControl.$itemNav);
 
-        // create the section nav and select the first section item
-        listControl.buildNavSection(listControl.$sections);
-        listControl.$navSection.find('li:first button').click();
+            // create the section nav and select the first section item
+            listControl.buildNavSection(listControl.$sections);
+            listControl.$navSection.find('li:first button').click();
 
-        // remove original HTML
-        listControl.$container.find('ul.sections').remove();
+            // remove original HTML
+            listControl.$container.find('ul.sections').remove();
 
-        // 3. set completed flag
-        listControl.initialised = true;
-    },
-    'buildNavSection' : function($sections) {
-        // sections i.e., first level nav <li>s
-        $sections.each(function() {
-            var $section = $(this);
-            var $li = $('<li/>');
+            // 3. set completed flag
+            listControl.initialised = true;
+        },
+        'buildNavSection' : function($sections) {
+            // sections i.e., first level nav <li>s
+            $sections.each(function() {
+                var $section = $(this);
+                var $li = $('<li/>');
 
-            $('<button/>')
-                // create button text from section h2
-                .text($section.find('h2:first').text())
+                $('<button/>')
+                    // create button text from section h2
+                    .text($section.find('h2:first').text())
 
-                // save reference to original section
-                .data('section', $section)
+                    // save reference to original section
+                    .data('section', $section)
+
+                    // bind behaviour
+                    .click(listControl.showSection)
+
+                    .appendTo($li);
+
+                // append to ul.section-nav
+                $li.appendTo(listControl.$navSection)
+            });
+        },
+        'buildItemNav' : function($items) {
+            // items i.e., second level nav <li>s
+            $items.each(function() {
+                var $item = $(this);
+                var $li = $('<li/>');
+            
+                $('<button/>')
+                // create button text from item h3
+                .text($item.find('h3:first').text())
+
+                // save reference to original item
+                .data('item', $item)
 
                 // bind behaviour
-                .click(listControl.showSection)
-
+                .click(listControl.showItemContent)
+                
                 .appendTo($li);
+                
+                // append to ul.item-nav
+                $li.appendTo(listControl.$itemNav)
+            });
+        },
+        'showSection' : function() { 
+            var $button = $(this);
+            var $li = $button.parent();
+            var $section = $button.data('section'); // set in buildNavSection
+            var $items = $section.find('ul li');
 
-            // append to ul.section-nav
-            $li.appendTo(listControl.$navSection)
-        });
-    },
-    'buildItemNav' : function($items) {
-        // items i.e., second level nav <li>s
-        $items.each(function() {
-            var $item = $(this);
-            var $li = $('<li/>');
-        
-            $('<button/>')
-              // create button text from item h3
-              .text($item.find('h3:first').text())
+            listControl.$itemNav.empty();
+            listControl.$content.empty();
 
-              // save reference to original item
-              .data('item', $item)
+            $li.addClass('current').siblings().removeClass('current');
 
-              // bind behaviour
-              .click(listControl.showItemContent)
-              
-              .appendTo($li);
-            
-            // append to ul.item-nav
-            $li.appendTo(listControl.$itemNav)
-        });
-    },
-    'showSection' : function() { 
-        var $button = $(this);
-        var $li = $button.parent();
-        var $section = $button.data('section'); // set in buildNavSection
-        var $items = $section.find('ul li');
+            // create the section items
+            listControl.buildItemNav($items);
 
-        listControl.$itemNav.empty();
-        listControl.$content.empty();
+            // select the first item
+            listControl.$itemNav.find('li:first button').click();
+        },
+        'showItemContent' : function() { 
+            var $button = $(this);
+            var $li = $button.parent();
+            var $item = $button.data('item'); // set in buildItemNav
 
-        $li.addClass('current').siblings().removeClass('current');
+            $li.addClass('current').siblings().removeClass('current');
 
-        // create the section items
-        listControl.buildItemNav($items);
+            // update .content with the item's HTML
+            listControl.$content.html($item.html());
+        }
+    };
 
-        // select the first item
-        listControl.$itemNav.find('li:first button').click();
-    },
-    'showItemContent' : function() { 
-        var $button = $(this);
-        var $li = $button.parent();
-        var $item = $button.data('item'); // set in buildItemNav
-
-        $li.addClass('current').siblings().removeClass('current');
-
-        // update .content with the item's HTML
-        listControl.$content.html($item.html());
-    }
-};
-
-// on DOM ready
-$(function () {
-    
     listControl.init();
 
     //listControl.init({ 'container' : $('.list-alternate') });
-});
+})()
